bdca15afc4f05f2f421b812094721d30e6a7ed4e
Kelsie <kelsie51@hotmail.com>
2021-04-01 22:48:29 -0500

    install inquirer, and base file arrangement

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+
+
+//const windex = require('./windex.js');  //readfile syntax used for append,read, write //modified fs to windex w/ ./ for indication of same diectory/not node specific
+//let globalData = "";
+//const readCallback = (err,data)=>{
+   //console.log(windex.questions);/// replaced ('data') with js.file and question variable
+   //console.log(windex.returnNumber1());  ///js file. and second fx ()///changed githubUrl to returnNumberone as example of changes you can make
+
+   
+
+//fs.readFile('data.csv','utf8', readCallback);// returning data.csv as string
+    
+
+//fs.writeFile('windex.js', process.argv[2], (err) =>//creates newFile from scratch,and adds text within
+    //err ? console.error(err) : console.log('success!')
+//);
+
+//fs.appendFile('log.txt', `${process.argv[2]}\n`, (err) =>//creates file, adds to process.argv a carriage return line break,error check  
+//err ? console.error(err) : console.log('Commit Logged')
+//);
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//const questions = [];
+////console.log(process.argv);
+
+/////const name1 = process.argv[2];
+/////const name2 = process.argv[3];
+////const extra = parseInt (process.argv[4]);////turning number into number,not string with parse
+//console.log(" user name is " + name1);
+////console.log(" another user name is " + name2); 
+////console.log(typeof extra);////turning number into number,not string with parse
+
+/////if (a===b) {///// if using conditional statement
+/////console.log(true);
+/////} else {
+//////console.log(false);
+//////}   
+
+/////console.log(a===b ? true : false) //////ternary operator
+/////console.log(a===b); /////for comparing variables
+/////console.log(process.agrv[2]===process.argv[3]);/////compares arguments directly
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+//function writeToFile(fileName, data) {}
+ 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
+//init();
---
+++ windex.js
@@ -0,0 +1,15 @@
+//const questions = 'userGithub';//variable of questions for usergitHub
+//const githubUrl = () => 1;//function to get githubUrl
+
+//const readFile = () => {////examples//
+
+//}
+//const writeFile = () => {////examples
+
+//}
+
+
+//module.exports = {questions : questions, returnNumber1:githubUrl,//////returned,seperated value questions and githuburl to same line, changed name to user specific.
+    //readFile,//
+    //writeFile,//
+//};
